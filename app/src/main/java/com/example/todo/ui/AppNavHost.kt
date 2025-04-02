411b3210d1faa7db573c71de61c2c6a089f26709
amanavelon <amanavelon@gmail.com>
2025-04-03 00:46:59 +0530

    Refactor: Update animations and add clickable modifier
    - Updated `AppNavHost` to add transitions for navigation, using `slideInHorizontally`, `slideOutHorizontally`, and `tween` for animation.
    - Updated `Todo` composable to add `clickable` modifier.

--- app/src/main/java/com/example/todo/ui/AppNavHost.kt
+++ app/src/main/java/com/example/todo/ui/AppNavHost.kt
@@ -1,5 +1,12 @@
 package com.example.todo.ui
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -22,21 +29,30 @@ fun AppNavHost(
         modifier = modifier,
         startDestination = SplashScreen
     ) {
-        composable<TodoScreen> {
+        composable<TodoScreen>(
+            enterTransition = { slideInHorizontally(initialOffsetX = { 1000 }, animationSpec = tween(500)) },
+            exitTransition = { slideOutHorizontally(targetOffsetX = { 2200 }, animationSpec = tween(500)) }
+        ) {
             val viewModel: TodoViewModel = koinViewModel()
             TodoScreen(navController, viewModel)
         }
-        composable<TodoDetailsScreen> {
+        composable<TodoDetailsScreen>(
+            exitTransition = { slideOutHorizontally(targetOffsetX = { -1200 }, animationSpec = tween(1000)) }
+        ) {
          val data = it.toRoute<TodoDetailsScreen>()
             TodoDetailsScreen(navController = navController, todoId = data.id)
         }
-        composable<SplashScreen> {
+        composable<SplashScreen>{
             SplashScreen(onAnimationFinish = {navController.navigate(TodoScreen)})
         }
-        composable<CreateTodo> {
+        composable<CreateTodo>(
+            enterTransition = { slideInHorizontally(initialOffsetX = { 1000 }, animationSpec = tween(500)) },
+            exitTransition = { slideOutHorizontally(targetOffsetX = { 2200 }, animationSpec = tween(500)) }
+        ) {
+
             val viewModel: TodoViewModel = koinViewModel()
             CreateTodo(onDismiss = { navController.popBackStack() }, viewModel)
         }
     }
-    
+
 }
--- app/src/main/java/com/example/todo/ui/screen/mainscreen/Todo.kt
+++ app/src/main/java/com/example/todo/ui/screen/mainscreen/Todo.kt
@@ -1,6 +1,8 @@
 package com.example.todo.ui.screen.mainscreen
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
